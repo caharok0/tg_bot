9ea43b234e6daa266b9c08c5713ea3c0857ae6f4
caharok0 <zvagincevkiril@gmail.com>
2025-08-10 13:36:54 +0300

    Delete tg_bot-main directory

--- tg_bot-main/tg_bot/kbrds/reply.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-
-help_kb = ReplyKeyboardBuilder()
-help_kb.add(
-    KeyboardButton(text="info"),
-    KeyboardButton(text="anekdot")
-)
